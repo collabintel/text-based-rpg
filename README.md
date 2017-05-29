0cce8e16b7b5c7b367bf92ee0ad57672f9b07e26
mderbent <mderbent@ebay.com>
2017-05-29 03:52:28 +0300

    Documentation completed

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # text-based-rpg
+
+**Welcome To The Dungeon Explorer**
+
+Prerequirements: 
+
+You need java 8 installed. 
+This game is built for the command line.
+
+
+-_**Build Game**_
+
+`./gradlew build`
+
+-_**Run Game**_
+
+`java -jar build/libs/text-based-rpg.jar`
+
+-_**Documentation**_
+
+You can find the documentation in doc folder.
+
+-_**Improvements For The Future**_
+
+
+- All text and properties should be read from a json property file.
+- Actions have high code complexity. Command pattern may be used for the business logic happens in there.
+- Code coverage should be increased.
+- Rooms should have descriptions. When you enter a room, you should see room's description. There should be a bunch of them loaded somewhere and while game was built, these should be appended to the rooms randomly.
+- Characters should be saved in file. Experiences should be saved to that file, too.
+- There should be a final room (Boss room if you might call).
+- After final room, there may be some more dungeons that player may want to visit. Or exit.
+- There may be items in rooms.
+- Enemies may drop some items.
+- Enemies created by their difficulties
--- src/main/java/mad/rpg/game/Messages.java
+++ src/main/java/mad/rpg/game/Messages.java
@@ -12,16 +12,16 @@ public class Messages {
     public static final String INVALID_COMMAND = "Invalid command, try again...";
     public static final String CHARACTER_ADDED = "Character added";
     public static final String CHARACTER_NOT_ADDED = "Character not added";
-    public static final String SAVE_CHARACTER_PROMPT = "Do you want to save this character?";
+    public static final String SAVE_CHARACTER_PROMPT = "Do you want to save this character? (yes-no)";
     public static final String CHARACTER_NOT_FOUND = "Character not found, please select an existing character.";
     public static final String CHARACTER_SELECTED = "%s selected";
     public static final String CANT_GO_THAT_WAY = "Can't go that way!";
     public static final String YOU_HAVE_ENTERED_THE_DUNGEON = "You have entered the dungeon...\nYou can explore it by going north, south, east and west.";
     public static final String YOU_ARE_IN_ROOM = "You are in room %s";
     public static final String ENEMY_FOUND_IN_THE_ROOM = "Enemy found in the room!";
-    public static final String WHERE_DO_YOU_WANT_TO_GO_NOW = "Where do you want to go now?";
+    public static final String WHERE_DO_YOU_WANT_TO_GO_NOW = "Where do you want to go now? (go north-south-east-west)";
     public static final String YOU_FACE_ENEMY_NAME = "You face %s, health: %s";
-    public static final String DO_YOU_WANT_TO_FIGHT_OR_RUN_AWAY = "Do you want to fight or run away?";
+    public static final String DO_YOU_WANT_TO_FIGHT_OR_RUN_AWAY = "Do you want to -fight- or -run away-?";
     public static final String YOU_RAN_AWAY_FROM_ENEMY = "You ran away from %s";
     public static final String YOU_LOST_HEALTH = "You lost %s health";
     public static final String YOU_DEALT_DAMAGE_TO_ENEMEY = "You dealt %s damage to the enemy";
@@ -30,7 +30,7 @@ public class Messages {
     public static final String YOU_GAINED_EXPERIENCE = "You gained %s experience, total experience: %s";
     public static final String YOU_HAVE_HEALTH = "You have %s health";
     public static final String DO_YOU_WANT_TO_START_A_NEW_GAME_OR_LOAD_GAME = "Do you want to start a -new game- or -load game-?";
-    public static final String DO_YOU_WANT_TO_SAVE_THIS_GAME = "Do you want to save this game?";
+    public static final String DO_YOU_WANT_TO_SAVE_THIS_GAME = "Do you want to save this game? (yes-no)";
 
     /**
      * Error Messages
